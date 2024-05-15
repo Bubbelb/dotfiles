5ef66ab24a4fb7448dbfbe2267a229ea879e39da
Bas Bleeker <b.c.j.bleeker@umcutrecht.nl>
2024-05-15 16:50:23 +0200

    Update .config/nvim/lua/plugins/telescope.lua

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -3,7 +3,7 @@
 -- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 return {
   { 'nvim-lua/plenary.nvim' },
-  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
+  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   {
     'nvim-telescope/telescope.nvim',
     dependencies = {
