340984943c018901c17cd1c0205e731cfa14ffa6
Bas Bleeker <b.c.j.bleeker@umcutrecht.nl>
2024-05-18 15:56:54 +0200

    Update .config/nvim/lua/plugins/theming.lua

--- dot_config/nvim/lua/plugins/theming.lua
+++ dot_config/nvim/lua/plugins/theming.lua
@@ -40,7 +40,7 @@ return {
               light_style = "day",
               transparent = false, -- Enable this to disable setting the background color
               terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
-              day_brightness = 0.9, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+              day_brightness = 0.1, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
               hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
               dim_inactive = true, -- dims inactive windows
               lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
