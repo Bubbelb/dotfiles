b3e1cf666f2789b85238b139cea431bd8a6d6900
Bas Bleeker <b.c.j.bleeker@umcutrecht.nl>
2024-05-27 00:44:55 +0200

    Update .config/nvim/lua/plugins/flatten.lua

--- dot_config/nvim/lua/plugins/flatten.lua
+++ dot_config/nvim/lua/plugins/flatten.lua
@@ -2,17 +2,14 @@ return {
     {
         'willothy/flatten.nvim',
         config = true,
-        -- or pass configuration with
-        -- opts = {  }
-        -- Ensure that it runs first to minimize delay when opening file from terminal
         lazy = false,
         priority = 1001,
     },
     {
         'nat-418/scamp.nvim',
-        config = function(_, opts)
-            require("scamp").setup(opts)
-        end,
-        lazy = false,
     },
+    {
+        'chentoast/marks.nvim',
+    },
+
 }
