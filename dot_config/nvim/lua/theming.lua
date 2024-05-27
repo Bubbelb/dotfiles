4cef22bac6b53001984fd543197706f7a59d270c
Bas Bleeker <b.c.j.bleeker@umcutrecht.nl>
2024-05-27 12:02:12 +0200

    Remove .config/nvim/lua/theming.lua

--- dot_config/nvim/lua/theming.lua
+++
@@ -1,9 +0,0 @@
--- Indent Guide options
---
--- https://github.com/kdheepak/tabline.nvim
---
-vim.g.indent_guides_color_change_percent = 5
-vim.g.indent_guides_enable_on_vim_startup = 1
-vim.g.indent_guides_exclude_filetypes = { 'md' }
-
-vim.opt.sessionoptions:append( { 'tabpages', 'globals' } ) -- store tabpages and globals in session
