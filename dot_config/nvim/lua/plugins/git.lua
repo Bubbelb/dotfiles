97f52a6ae80a80de0da6661f287eaae4ec726413
Bas Bleeker <bb_subscr@bubbel.org>
2024-05-31 14:12:58 +0200

    Update .config/nvim/lua/plugins/git.lua

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -1,13 +1,16 @@
 return {
-{
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim",         -- required
-    "sindrets/diffview.nvim",        -- optional - Diff integration
-
-    -- Only one of these is needed, not both.
-    "nvim-telescope/telescope.nvim", -- optional
-  },
-  config = true
-}
+    {
+        "NeogitOrg/neogit",
+        keys = {
+            { "<Leader>go", "<cmd>Neogit<cr>",           desc = "Open the Neogit status buffer." },
+            { "<Leader>gf", "<cmd>Neogit cwd=%:p:h<cr>", desc = "Uses the repository of the current file." },
+            { "<Leader>gc", "<cmd>Neogit commit<cr>",    desc = "Open commit popup." },
+        },
+        dependencies = {
+            "nvim-lua/plenary.nvim",  -- required
+            "sindrets/diffview.nvim", -- optional - Diff integration
+            "nvim-telescope/telescope.nvim", -- optional
+        },
+        config = true
+    }
 }
