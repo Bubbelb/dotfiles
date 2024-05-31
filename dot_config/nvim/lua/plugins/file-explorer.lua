ccdd12ad9c87734eee3cd0de2e1f2d202b6f934b
Bas Bleeker <bb_subscr@bubbel.org>
2024-05-31 16:27:18 +0200

    Update .config/nvim/lua/plugins/file-explorer.lua

--- dot_config/nvim/lua/plugins/file-explorer.lua
+++ dot_config/nvim/lua/plugins/file-explorer.lua
@@ -2,23 +2,45 @@
 -- File Explorer
 -- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 return {
-  {
-    'nvim-neo-tree/neo-tree.nvim',
-    keys = {
-      {
-        "<Leader>e",
-        "<cmd>Neotree toggle<cr>",
-        desc = "NeoTree"
-      },
+    -- {
+    --   'nvim-neo-tree/neo-tree.nvim',
+    --   keys = {
+    --     {
+    --       "<Leader>e",
+    --       "<cmd>Neotree toggle<cr>",
+    --       desc = "NeoTree"
+    --     },
+    --   },
+    --   config = function()
+    --     require("neo-tree").setup()
+    --   end,
+    --   dependencies = {
+    --     'nvim-tree/nvim-web-devicons',
+    --     "nvim-lua/plenary.nvim",
+    --     "MunifTanjim/nui.nvim",
+    --   },
+    -- },
+    {
+        'nvim-tree/nvim-tree.lua',
+        keys = {
+            {
+                "<Leader>e",
+                "<cmd>NvimTreeToggle<cr>",
+                desc = "NeoTree"
+            },
+        },
+        opts = {
+            sync_root_with_cwd = true,
+            respect_buf_cwd = true,
+            update_focused_file = {
+                enable = true,
+                update_root = true
+            },
+        },
+        dependencies = {
+            'nvim-tree/nvim-web-devicons',
+            "nvim-lua/plenary.nvim",
+            -- "MunifTanjim/nui.nvim",
+        },
     },
-    config = function()
-      require("neo-tree").setup()
-    end,
-    dependencies = {
-      'nvim-tree/nvim-web-devicons',
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-    },
-  },
 }
-
