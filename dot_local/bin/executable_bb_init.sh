7bdabf23d9f16086f9132c70f172e89db54af3f0
Bas Bleeker <bb_subscr@bubbel.org>
2024-06-11 12:57:16 +0200

    Update .local/bin/bb_init.sh

--- dot_local/bin/executable_bb_init.sh
+++ dot_local/bin/executable_bb_init.sh
@@ -1,31 +1,34 @@
 if [ $(id -nu) == "bbl" ] ; then
-	echo "## Chezmoi Update: Pulling latest from GitHub"
-	chezmoi git pull
+    do_bb_init_updates() {
+        echo "## Chezmoi Update: Pulling latest from GitHub"
+        chezmoi git pull
 
-	# get a list of files to update that don't have local changes
-	FILES=$(chezmoi status | awk '/^ / {print $2}')
+        # get a list of files to update that don't have local changes
+        FILES=$(chezmoi status | awk '/^ / {print $2}')
 
-	echo -e "\n## Chezmoi Update: Updating files without local changes"
+        echo -e "\n## Chezmoi Update: Updating files without local changes"
 
-	for f in $FILES; do
-	  chezmoi apply ~/${f}
-	done
+        for f in $FILES; do
+          chezmoi apply ~/${f}
+        done
 
-	echo -e "\n## Chezmoi Update: Files with local changes that need adding"
-	chezmoi status 1>&2
+        echo -e "\n## Chezmoi Update: Files with local changes that need adding"
+        chezmoi status 1>&2
 
-	echo -e "\n## Neovim: Update Plugins"
-	nvim --headless "+Lazy! sync" +qa
-	echo -e "\n## Neovim: Update Language Servers (Mason)"
-	nvim --headless "+MasonUpdate" +qa
-	echo -e "\n## Neovim: Update TreeSitter Languages"
-	nvim --headless "+TSUpdateSync" +qa
+        echo -e "\n## Neovim: Update Plugins"
+        nvim --headless "+Lazy! sync" +qa
+        echo -e "\n## Neovim: Update Language Servers (Mason)"
+        nvim --headless "+MasonUpdate" +qa
+        echo -e "\n## Neovim: Update TreeSitter Languages"
+        nvim --headless "+TSUpdateSync" +qa
 
-	echo 
-	if [ ~/.local/bin/distrobox ] ; then
-          ~/.local/bin/check-interval -n distrobox-update -i 168 -c 'echo "Distrobox: Update distrobox User install" \
-          curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --prefix ~/.local'
-	fi
+        echo
+        if [ ~/.local/bin/distrobox ] ; then
+              ~/.local/bin/check-interval -n distrobox-update -i 168 -c 'echo "Distrobox: Update distrobox User install" \
+              curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --prefix ~/.local'
+        fi
+    }
+    do_bb_init_updates &
 else
 	echo "## Switching to user 'bbl'"
 	sudo -u bbl "$0" "$*"
