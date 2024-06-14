cec9343ed185659581c6b37056db89785bb81346
Bas Bleeker <bb_subscr@bubbel.org>
2024-06-14 15:07:03 +0200

    Update .config/nvim/lua/opt.lua

--- dot_config/nvim/lua/opt.lua
+++ dot_config/nvim/lua/opt.lua
@@ -40,3 +40,25 @@ end
 -- Setup the spell checker
 vim.opt.spell = true
 vim.opt.spelllang = 'en_gb'
+
+-- Toggle Diagnostics
+vim.g.diagnostics_active = true
+function _G.toggle_diagnostics()
+  if vim.g.diagnostics_active then
+    vim.g.diagnostics_active = false
+    vim.lsp.diagnostic.clear(0)
+    vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
+  else
+    vim.g.diagnostics_active = true
+    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+      vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = true,
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+      }
+    )
+  end
+end
+
+vim.api.nvim_set_keymap('n', '<leader>xD', ':call v:lua.toggle_diagnostics()<CR>',  {noremap = true, silent = true})
