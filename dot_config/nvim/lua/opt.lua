a99234be3ef0aa1f0ce016c367a090fe776ed258
Bas Bleeker <bb_subscr@bubbel.org>
2024-06-14 10:12:10 +0200

    Update .config/nvim/lua/opt.lua

--- dot_config/nvim/lua/opt.lua
+++ dot_config/nvim/lua/opt.lua
@@ -36,3 +36,7 @@ for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl= hl, numhl = hl })
 end
+
+-- Setup the spell checker
+vim.opt.spell = true
+vim.opt.spelllang = 'en_gb'
