4d9a43ff7614715a57b3e57cd8cdeb34a4ede3f2
Bas Bleeker <bb_subscr@bubbel.org>
2024-06-14 12:06:18 +0200

    Update .config/nvim/lua/plugins/lsp-ts.lua

--- dot_config/nvim/lua/plugins/lsp-ts.lua
+++ dot_config/nvim/lua/plugins/lsp-ts.lua
@@ -100,36 +100,34 @@ return {
             "BufReadPre",
             "BufNewFile",
         },
-    config = function()
-      local lint = require("lint")
+        config = function()
+            local lint = require("lint")
 
-      lint.linters_by_ft = {
-        javascript = { "eslint_d" },
-        typescript = { "eslint_d" },
-        javascriptreact = { "eslint_d" },
-        typescriptreact = { "eslint_d" },
-        svelte = { "eslint_d" },
-        kotlin = { "ktlint" },
-        terraform = { "tflint" },
-        markdown = { "markdownlint" },
-        yaml = { "yamllint" },
-        json = { "jsonlint" },
-      }
+            lint.linters_by_ft = {
+                javascript = { "eslint_d" },
+                typescript = { "eslint_d" },
+                javascriptreact = { "eslint_d" },
+                typescriptreact = { "eslint_d" },
+                svelte = { "eslint_d" },
+                kotlin = { "ktlint" },
+                terraform = { "tflint" },
+                markdown = { "markdownlint" },
+                yaml = { "yamllint" },
+                json = { "jsonlint" },
+            }
+            local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+            vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+                group = lint_augroup,
+                callback = function()
+                  lint.try_lint()
+                end,
+            })
 
-      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-        group = lint_augroup,
-        callback = function()
-          lint.try_lint()
+            vim.keymap.set("n", "<leader>ll", function()
+                lint.try_lint()
+            end, { desc = "Trigger linting for current file" })
         end,
-      })
-
-      vim.keymap.set("n", "<leader>ll", function()
-        lint.try_lint()
-      end, { desc = "Trigger linting for current file" })
-    end,
-         },
         dependencies = {
             "rshkarin/mason-nvim-lint",
         },
