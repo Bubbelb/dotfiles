8a6fed2033419b7914fb4b1c263fa0ce87b21639
Bas Bleeker <bb_subscr@bubbel.org>
2024-06-17 15:21:20 +0200

    Update .config/nvim/lua/plugins/lsp-ts.lua

--- dot_config/nvim/lua/plugins/lsp-ts.lua
+++ dot_config/nvim/lua/plugins/lsp-ts.lua
@@ -116,9 +116,6 @@ return {
                 yaml = { "yamllint" },
                 json = { "jsonlint" },
             }
-        local yllint = require('lint').linters.yamllint
-        yllint.args = { "-d", '"{extends: default, rules: {line-length: {max: 120}}}"' }
-
         local mdlint = require('lint').linters.markdownlint
         mdlint.args = { "--disable", "MD013", "--" }
 
