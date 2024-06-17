c26a47b03679ced180d383c9d7215c8e186ca8ce
Bas Bleeker <bb_subscr@bubbel.org>
2024-06-17 16:07:02 +0200

    Update .config/nvim/lua/plugins/misc.lua

--- dot_config/nvim/lua/plugins/misc.lua
+++ dot_config/nvim/lua/plugins/misc.lua
@@ -20,10 +20,16 @@ return {
     },
     {
         'gorbit99/codewindow.nvim',
+        keys = {
+          { "<Leader>mo", "<cmd>lua require('codewindow').open_minimap()<cr>", desc = "Open MiniMap" },
+          { "<Leader>mc", "<cmd>lua require('codewindow').close_minimap()<cr>", desc = "Close MiniMap" },
+          { "<Leader>mf", "<cmd>lua require('codewindow').toggle_focus()<cr>", desc = "(Un-)Focus MiniMap" },
+          { "<Leader>mm", "<cmd>lua require('codewindow').toggle_minimap()<cr>", desc = "Toggle MiniMap" },
+        },
         config = function()
-            local codewindow = require('codewindow')
-            codewindow.setup()
-            codewindow.apply_default_keybinds()
-        end,
+             local codewindow = require('codewindow')
+             codewindow.setup()
+        --     codewindow.apply_default_keybinds()
+         end,
     },
 }
