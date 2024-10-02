f16451f26088e6fa1fb49b605d3fe5a47d55694e
Bas Bleeker <bb_subscr@bubbel.org>
2024-10-02 18:31:35 +0200

    Update .config/nvim/lua/plugins/todo-comments.lua

--- dot_config/nvim/lua/plugins/todo-comments.lua
+++ dot_config/nvim/lua/plugins/todo-comments.lua
@@ -10,7 +10,27 @@ return {
     },
     {
         "Djancyp/better-comments.nvim",
-        lazy = false
+        lazy = false,
+        config = function()
+            require('better-comment').Setup({
+                tags = {
+                    {
+                        name = "TODO",
+                        fg = "white",
+                        bg = "#0a7aca",
+                        bold = true,
+                        virtual_text = "",
+                    },
+                    {
+                        name = "NEW",
+                        fg = "white",
+                        bg = "red",
+                        bold = false,
+                        virtual_text = "",
+                    },
+                }
+            })
+        end
     }
 }
 
