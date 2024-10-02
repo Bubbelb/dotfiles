008db22031c3c8f38e705f523e745d17604382de
Bas Bleeker <bb_subscr@bubbel.org>
2024-10-02 18:41:42 +0200

    Update .config/nvim/lua/plugins/todo-comments.lua

--- dot_config/nvim/lua/plugins/todo-comments.lua
+++ dot_config/nvim/lua/plugins/todo-comments.lua
@@ -11,45 +11,47 @@ return {
     {
         "Djancyp/better-comments.nvim",
         lazy = false,
-        opts = {
-            tags = {
-                {
-                    name = "TODO",
-                    fg = "white",
-                    bg = "#0a7aca",
-                    bold = true,
-                    virtual_text = "",
-                },
-                {
-                    name = "FIX",
-                    fg = "white",
-                    bg = "#f44747",
-                    bold = true,
-                    virtual_text = "This is virtual Text from FIX",
-                },
-                {
-                    name = "WARNING",
-                    fg = "#FFA500",
-                    bg = "",
-                    bold = false,
-                    virtual_text = "This is virtual Text from WARNING",
-                },
-                {
-                    name = "?",
-                    fg = "lightblue",
-                    bg = "",
-                    bold = true,
-                    virtual_text = "",
-                },
-                {
-                    name = "!",
-                    fg = "#f44747",
-                    bg = "",
-                    bold = true,
-                    virtual_text = "",
+        config = function()
+            require('better-comment').Setup({
+                tags = {
+                    {
+                        name = "TODO",
+                        fg = "white",
+                        bg = "#0a7aca",
+                        bold = true,
+                        virtual_text = "",
+                    },
+                    {
+                        name = "FIX",
+                        fg = "white",
+                        bg = "#f44747",
+                        bold = true,
+                        virtual_text = "This is virtual Text from FIX",
+                    },
+                    {
+                        name = "WARNING",
+                        fg = "#FFA500",
+                        bg = "",
+                        bold = false,
+                        virtual_text = "This is virtual Text from WARNING",
+                    },
+                    {
+                        name = "?",
+                        fg = "lightblue",
+                        bg = "",
+                        bold = true,
+                        virtual_text = "",
+                    },
+                    {
+                        name = "!",
+                        fg = "#f44747",
+                        bg = "",
+                        bold = true,
+                        virtual_text = "",
+                    }
                 }
-            }
-        }
+            })
+        end
     }
 }
 
