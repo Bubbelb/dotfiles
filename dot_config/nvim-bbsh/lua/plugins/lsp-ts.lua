4af46baee6a4fc17e3ec6bbb3b5d05fe04f7685a
Bas Bleeker <bb_subscr@bubbel.org>
2024-12-12 10:59:17 +0100

    Update .config/nvim-bbsh/lua/plugins/lsp-ts.lua

--- dot_config/nvim-bbsh/lua/plugins/lsp-ts.lua
+++ dot_config/nvim-bbsh/lua/plugins/lsp-ts.lua
@@ -32,18 +32,12 @@ return {
         'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
         opts = {
-            ensure_installed = { all },
+            ensure_installed = { "all" },
             sync_install = false,
             auto_install = true,
             highlight = {
                 enable = true,
                 additional_vim_regex_highlighting = false,
-            },
-            rainbow = {
-                enable = true,
-                query = 'rainbow-parens',
-                extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-                max_file_lines = nil, -- Do not enable for files with more than n lines, int
             },
         },
         config = function(_, opts)
