9c7b501731d17db3ff4bb076468f73f2fbc68b8b
Bas Bleeker <bb_subscr@bubbel.org>
2024-12-19 21:31:53 +0100

    Remove .config/nvim-bbsh/init.lua Remove .config/nvim-bbsh/lua/lsp.lua Remove .config/nvim-bbsh/lua/mappings.lua Remove .config/nvim-bbsh/lua/nvim-cmp.lua Remove .config/nvim-bbsh/lua/opt.lua Remove .config/nvim-bbsh/lua/plugins/autosave.lua Remove .config/nvim-bbsh/lua/plugins/dashboard.lua Remove .config/nvim-bbsh/lua/plugins/file-explorer.lua Remove .config/nvim-bbsh/lua/plugins/floating-terminal.lua Remove .config/nvim-bbsh/lua/plugins/git.lua Remove .config/nvim-bbsh/lua/plugins/lsp-ts.lua Remove .config/nvim-bbsh/lua/plugins/misc.lua Remove .config/nvim-bbsh/lua/plugins/project.lua Remove .config/nvim-bbsh/lua/plugins/telescope.lua Remove .config/nvim-bbsh/lua/plugins/theming.lua Remove .config/nvim-bbsh/lua/plugins/todo-comments.lua Remove .config/nvim-bbsh/lua/plugins/trouble.lua Remove .config/nvim-bbsh/lua/plugins/which-key.lua

--- dot_config/nvim-bbsh/lua/plugins/floating-terminal.lua
+++
@@ -1,9 +0,0 @@
-
-return {
- { 'numToStr/FTerm.nvim',
-      keys = {
-        { "<C-t>", "<cmd>lua require('FTerm').toggle()<cr>", desc = "Toggle Terminal Window" },
-        { "<C-t>", "<cmd>lua require('FTerm').toggle()<cr>", 'i', desc = "Toggle Terminal Window" },
-      },
- },
-}
