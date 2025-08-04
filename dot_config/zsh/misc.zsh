16434edf860f4bcd90919d3dce2afb8e69999ce5
Bas Bleeker <1848081+Bubbelb@users.noreply.github.com>
2025-08-04 13:48:00 +0200

    Update .config/zsh/misc.zsh

--- dot_config/zsh/misc.zsh
+++ dot_config/zsh/misc.zsh
@@ -114,55 +114,57 @@ function _ssh {
 
 compdef _ssh ssh
 
-# Options to fzf command
-export FZF_COMPLETION_OPTS=''
+if command -p fzf >/dev/null 2>&1 ; then
+    # Options to fzf command
+    export FZF_COMPLETION_OPTS=''
 
-# Use fd (https://github.com/sharkdp/fd) instead of the default find
-# command for listing path candidates.
-# - The first argument to the function ($1) is the base path to start traversal
-# - See the source code (completion.{bash,zsh}) for the details.
-_fzf_compgen_path() {
-  if [[ -x /usr/bin/fdfind ]] ; then
-      fdfind --hidden --follow --exclude ".git" . "$1"
-  else
-      fd --hidden --follow --exclude ".git" . "$1"
-  fi
-}
+    # Use fd (https://github.com/sharkdp/fd) instead of the default find
+    # command for listing path candidates.
+    # - The first argument to the function ($1) is the base path to start traversal
+    # - See the source code (completion.{bash,zsh}) for the details.
+    _fzf_compgen_path() {
+      if [[ -x /usr/bin/fdfind ]] ; then
+          fdfind --hidden --follow --exclude ".git" . "$1"
+      else
+          fd --hidden --follow --exclude ".git" . "$1"
+      fi
+    }
 
-# Use fd to generate the list for directory completion
-_fzf_compgen_dir() {
-  if [[ -x /usr/bin/fdfind ]] ; then
-      fdfind --type d --hidden --follow --exclude ".git" . "$1"
-  else
-      fd --type d --hidden --follow --exclude ".git" . "$1"
-  fi
-}
+    # Use fd to generate the list for directory completion
+    _fzf_compgen_dir() {
+      if [[ -x /usr/bin/fdfind ]] ; then
+          fdfind --type d --hidden --follow --exclude ".git" . "$1"
+      else
+          fd --type d --hidden --follow --exclude ".git" . "$1"
+      fi
+    }
 
-# Advanced customization of fzf options via _fzf_comprun function
-# - The first argument to the function is the name of the command.
-# - You should make sure to pass the rest of the arguments to fzf.
-_fzf_comprun() {
-  local command=$1
-  shift
+    # Advanced customization of fzf options via _fzf_comprun function
+    # - The first argument to the function is the name of the command.
+    # - You should make sure to pass the rest of the arguments to fzf.
+    _fzf_comprun() {
+      local command=$1
+      shift
 
-  case "$command" in
-    cd)           fzf --preview 'tree -C {} | head -200'   "$@" ;;
-    export|unset) fzf --preview "eval 'echo \$'{}"         "$@" ;;
-    ssh)          fzf --preview 'dig {}'                   "$@" ;;
-    *)            if [[ -x /usr/bin/batcat ]] ; then
-                    fzf --preview 'batcat -n --color=always {}' "$@"
-                  else
-                    fzf --preview 'bat -n --color=always {}' "$@"
-                  fi ;;
-  esac
-}
-source "${ZDOTDIR}/fzf-zsh-completion.sh"
-bindkey '^I' fzf_completion
-zstyle ':completion:*' fzf-search-display true
-if [[ -x /usr/bin/batcat ]] ; then
-    zstyle ':completion::*:ls::*' fzf-completion-opts --preview='eval batcat --color=always -n {1}'
-else
-    zstyle ':completion::*:ls::*' fzf-completion-opts --preview='eval bat --color=always -n {1}'
+      case "$command" in
+        cd)           fzf --preview 'tree -C {} | head -200'   "$@" ;;
+        export|unset) fzf --preview "eval 'echo \$'{}"         "$@" ;;
+        ssh)          fzf --preview 'dig {}'                   "$@" ;;
+        *)            if [[ -x /usr/bin/batcat ]] ; then
+                        fzf --preview 'batcat -n --color=always {}' "$@"
+                      else
+                        fzf --preview 'bat -n --color=always {}' "$@"
+                      fi ;;
+      esac
+    }
+    source "${ZDOTDIR}/fzf-zsh-completion.sh"
+    bindkey '^I' fzf_completion
+    zstyle ':completion:*' fzf-search-display true
+    if [[ -x /usr/bin/batcat ]] ; then
+        zstyle ':completion::*:ls::*' fzf-completion-opts --preview='eval batcat --color=always -n {1}'
+    else
+        zstyle ':completion::*:ls::*' fzf-completion-opts --preview='eval bat --color=always -n {1}'
+    fi
 fi
 
 #OS Data
