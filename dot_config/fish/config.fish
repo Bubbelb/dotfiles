114f3104dc9e71b5d80cb2dc699eb0e2e451c4ec
Bas Bleeker <1848081+Bubbelb@users.noreply.github.com>
2025-10-05 20:36:32 +0200

    Add .config/fish/config.fish Add .config/fish/fish_plugins Add .config/fish/fish_variables

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,9 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+#
+theme_gruvbox dark medium
