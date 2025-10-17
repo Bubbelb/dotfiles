32e576e6ff8743b6cb0ddc4a144897075dad32c2
Bas Bleeker <1848081+Bubbelb@users.noreply.github.com>
2025-10-17 23:39:36 +0200

    Update .config/fish/c-config.fish

--- dot_config/fish/c-config.fish
+++ dot_config/fish/c-config.fish
@@ -8,10 +8,6 @@ function fish_greeting
     fastfetch
 end
 
-# Format man pages
-set -x MANROFFOPT "-c"
-set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
-
 # Set settings for https://github.com/franciscolourenco/done
 set -U __done_min_cmd_duration 10000
 set -U __done_notification_urgency_level low
