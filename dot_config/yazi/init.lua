43de3e50d1bf775b7486ba4a829c2f57a1a82a1f
Bas Bleeker <1848081+Bubbelb@users.noreply.github.com>
2025-10-18 22:53:10 +0200

    Update .config/yazi/init.lua

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -46,3 +46,10 @@ Status:children_add(function(self)
 		return ""
 	end
 end, 3300, Status.LEFT)
+
+Header:children_add(function()
+	if ya.target_family() ~= "unix" then
+		return ""
+	end
+	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+end, 500, Header.LEFT)
