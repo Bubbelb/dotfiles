c5bcf395ef8079bd4e4fe8b679145e420e582bd2
Bas Bleeker <1848081+Bubbelb@users.noreply.github.com>
2025-10-20 21:03:17 +0200

    Update .config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,5 +1,9 @@
 source $__fish_config_dir/c-config.fish
 
+if not test -f $HOME/.config/fish/functions/fisher.fish
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+end
+
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
