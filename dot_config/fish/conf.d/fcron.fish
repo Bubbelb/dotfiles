7166b2300a51adbc4582dcbbc1ebf4510bb6e73f
Bas Bleeker <b.c.j.bleeker@umcutrecht.nl>
2025-10-24 10:44:07 +0200

    Update .config/fish/conf.d/fcron.fish

--- dot_config/fish/conf.d/fcron.fish
+++ dot_config/fish/conf.d/fcron.fish
@@ -1,3 +1,3 @@
-if test (pgrep -c fcron) -gt 0 ; and type -q fcrontab ; and fcrontab -l 2>&1 | grep -Fq "INFO user $USER has no fcrontab."
+if pgrep fcron >/dev/null ; and type -q fcrontab ; and fcrontab -l 2>&1 | grep -Fq "INFO user $USER has no fcrontab."
     fcrontab $HOME/.config/fcrontab 2>/dev/null
 end
