57e732fdc25d8305f019a12c01f2dd4c43b026dc
Bas Bleeker <b.c.j.bleeker@umcutrecht.nl>
2025-12-09 13:35:10 +0100

    Update .config/fish/conf.d/_bw_init.fish

--- dot_config/fish/conf.d/_bw_init.fish
+++ dot_config/fish/conf.d/_bw_init.fish
@@ -1,5 +1,7 @@
 # Bitwarden SSH integration
-if test -S "$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock"
+if test -S "$HOME/.bitwarden-ssh-agent.sock"
+     set -g --erase SSH_AUTH_SOCK
+     set -Ux SSH_AUTH_SOCK "$HOME/.bitwarden-ssh-agent.sock"
+ else if test -S "$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock"
      set -g --erase SSH_AUTH_SOCK
      set -Ux SSH_AUTH_SOCK "$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock"
- end
