94896a5c40b1d257119dd31f3dce17dbdadb10d7
Bubbelb <1848081+Bubbelb@users.noreply.github.com>
2026-01-23 14:03:35 +0100

    Update bbsh.sh

--- dot_config/distrobox/bbsh.sh
+++ dot_config/distrobox/bbsh.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-# function install_ouch() {
-#	if ! [[ -f /usr/local/bin/ouch ]] ; then 
-#		wget -qO- "$(wget -qO- https://api.github.com/repos/ouch-org/ouch/releases/latest \
-#			| jq -r '.assets[] | select( .name | match("'$(uname -m)'-unknown-linux-musl\\.tar\\.gz$") | .browser_download_url')" \
-#		    | tar xz -C /usr/local/bin/ --strip-components=2
-#	fi
-#}
-
-function install_bat_extras() {
-	if ! [[ -f /usr/local/bin/batman ]] ; then
-		mkdir -p /tmp/bat-extras
-		wget -qO- "$(wget -qO- https://api.github.com/repos/eth-p/bat-extras/releases/latest | jq -r '.tarball_url')" \
-			| tar -xz -C /tmp/bat-extras --strip-components=1 && \
-				/tmp/bat-extras/build.sh --prefix=/usr/local/bin/ --install
+ function install_ouch() {
+	if ! [[ -f /usr/local/bin/ouch ]] ; then 
+		wget -qO- "$(wget -qO- https://api.github.com/repos/ouch-org/ouch/releases/latest \
+			| jq -r '.assets[] | select( .name | match("'$(uname -m)'-unknown-linux-musl\\.tar\\.gz$")) | .browser_download_url')" \
+		    | tar xz -C /usr/local/bin/ --strip-components=2
 	fi
 }
+
+# function install_bat_extras() {
+#	if ! [[ -f /usr/local/bin/batman ]] ; then
+#		mkdir -p /tmp/bat-extras
+#		wget -qO- "$(wget -qO- https://api.github.com/repos/eth-p/bat-extras/releases/latest | jq -r '.tarball_url')" \
+#			| tar -xz -C /tmp/bat-extras --strip-components=1 && \
+#				/tmp/bat-extras/build.sh --prefix=/usr/local/bin/ --install
+#	fi
+# }
 		
 
 # Main
