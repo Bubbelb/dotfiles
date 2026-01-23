4cf9620cd33935928ca1fa8b961f4793f79c1073
Bas Bleeker <b.c.j.bleeker@umcutrecht.nl>
2026-01-23 15:52:56 +0100

    Update .config/fish/conf.d/_manpager_init.fish

--- dot_config/fish/conf.d/_manpager_init.fish
+++ dot_config/fish/conf.d/_manpager_init.fish
@@ -1,7 +1,12 @@
-if type -q bat
-  set -Ux MANPAGER "bat -plman"
+if type -q most
+  set -Ux MANPAGER "most"
   set -Ux MANROFFOPT "-c"
-  set -Ux BAT_PAGER "less -RFK"
+  function man
+      MANWIDTH=(tput cols) man $argv
+  end
+end
+
+if type -q bat
   abbr -a --position anywhere -- --help '--help | bat -plhelp'
   abbr -a --position anywhere -- -h '-h | bat -plhelp'
 end
