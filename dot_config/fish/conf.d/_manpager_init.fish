deff5a09595c9e8c9d319576c299fe84349a78f4
Bas Bleeker <b.c.j.bleeker@umcutrecht.nl>
2026-01-23 14:45:45 +0100

    Update .config/fish/conf.d/_manpager_init.fish

--- dot_config/fish/conf.d/_manpager_init.fish
+++ dot_config/fish/conf.d/_manpager_init.fish
@@ -1,6 +1,3 @@
 if type -q bat
-  set --erase -U MANPAGER
-  set -Ux MANPAGER env\ BATMAN_IS_BEING_MANPAGER=yes\ bash\ (which batman)
-  set -Ux MANROFFOPT -c
-  set --erase -U MANROFFOPT
+  set -Ux MANPAGER "bat -plman"
 end
