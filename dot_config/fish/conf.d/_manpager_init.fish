ab0ffce6c5d87dc9a6ac9871f88f2de0ebdaf5ed
Bas Bleeker <b.c.j.bleeker@umcutrecht.nl>
2026-01-23 14:48:21 +0100

    Update .config/fish/conf.d/_manpager_init.fish

--- dot_config/fish/conf.d/_manpager_init.fish
+++ dot_config/fish/conf.d/_manpager_init.fish
@@ -1,4 +1,6 @@
 if type -q bat
   set -Ux MANPAGER "bat -plman"
   set -Ux MANROFFOPT -c
+  abbr -a --position anywhere -- --help '--help | bat -plhelp'
+  abbr -a --position anywhere -- -h '-h | bat -plhelp'
 end
