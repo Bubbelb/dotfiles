99c0e1b21e6d56a9d1671bdf488dbec5ee92e621
Bas Bleeker <1848081+Bubbelb@users.noreply.github.com>
2026-02-20 17:23:56 +0100

    Update .config/fish/conf.d/alias-abbr.fish

--- dot_config/fish/conf.d/alias-abbr.fish
+++ dot_config/fish/conf.d/alias-abbr.fish
@@ -1,7 +1,7 @@
 ﻿## Useful aliases
 # Replace ls with eza
-alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
-alias la='eza -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ls='eza --color=always --group-directories-first --icons' # preferred listing
+alias la='eza -la --color=always --group-directories-first --icons'  # all files and dirs
 alias ll='eza -l --color=always --group-directories-first --icons'  # long format
 alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
 alias l.="eza -a | grep -e '^\.'"                                     # show only dotfiles
