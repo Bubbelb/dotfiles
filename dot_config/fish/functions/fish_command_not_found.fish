368a20b707068d730d3e5f497b7a0a5214a0fc59
Bas Bleeker <1848081+Bubbelb@users.noreply.github.com>
2026-06-19 11:29:28 +0200

    Update .config/fish/functions/fish_command_not_found.fish

--- dot_config/fish/functions/fish_command_not_found.fish
+++ dot_config/fish/functions/fish_command_not_found.fish
@@ -1,7 +1,13 @@
 function fish_command_not_found
     # "In a container" check
     if grep -qsFx 'name="bbsh"' /run/.containerenv
-        distrobox-host-exec $argv
+        if distrobox-host-exec /usr/bin/env sh -c "command -v $argv[1]" &> /dev/null
+            echo "Running '$argv[1]' on host OS." >&2
+            distrobox-host-exec $argv
+        else
+            echo "Command '$argv[1]' not fond in bbsh nor host OS." >&2
+            return 127
+        end
     else
         __fish_default_command_not_found_handler $argv
     end
