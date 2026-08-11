34a6013b7ea9315305c3478ecc8167703f14f408
Bubbelb <1848081+Bubbelb@users.noreply.github.com>
2026-08-11 22:05:29 +0200

    Create run_tide_init.sh

---
+++ dot_config/fish/run_tide_init.sh
@@ -0,0 +1,13 @@
+tide configure --auto \
+               --style=Rainbow \
+               --prompt_colors='True color' \
+               --show_time='24-hour format' \
+               --rainbow_prompt_separators=Round \
+               --powerline_prompt_heads=Sharp \
+               --powerline_prompt_tails=Round \
+               --powerline_prompt_style='Two lines, character and frame' \
+               --prompt_connection=Disconnected \
+               --powerline_right_prompt_frame=No \
+               --prompt_connection_andor_frame_color=Light \
+               --prompt_spacing=Sparse --icons='Many icons' \
+               --transient=No
