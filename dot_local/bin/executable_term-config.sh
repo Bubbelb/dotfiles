df6b4e2cb5d460f599a7f930a5ddcba3ec6153ae
Bas Bleeker <1848081+Bubbelb@users.noreply.github.com>
2026-08-20 16:13:31 +0200

    Update .local/bin/term-config.sh

--- dot_local/bin/executable_term-config.sh
+++ dot_local/bin/executable_term-config.sh
@@ -28,24 +28,32 @@ PKGS_ALPINE_EDGE_MAIN=( lua5.5-libs )
 PKGS_ALPINE_EDGE_COMMUNITY=( yazi yazi-cli )
 PKGS_ALPINE_EDGE_TESTING=( ouch )
 
+ONLY_REPORT=0
+CHEZMOI_REPO=Bubbelb
+
 function arch-install-pkg() {
     echo "Checking missing packages and installing them..."
     MLIST=($(comm -23 <(echo "${PKGS_ARCH[@]}" | tr -s ' ' $'\n' | sort -u) <(pacman -Qsq | sort)))
-    if [[ "${#MLIST[@]}" -gt 0 ]] ; then
-        echo "Missing packages. Installing them."
-        sudo pacman -S --noconfirm ${MLIST[@]}
-		MLIST=($(comm -23 <(echo "${PKGS_ARCH[@]}" | tr -s ' ' $'\n' | sort -u) <(pacman -Qsq | sort)))
-		if [[ "${#MLIST[@]}" -gt 0 ]] ; then
-			echo "Not all packages installed as planed."
-			return 1
-		fi
-    else
-        echo "All packages are accounted for."
+    if [[ "${ONLY_REPORT}" == 0 ]] ; then
+        if [[ "${#MLIST[@]}" -gt 0 ]] ; then
+            echo "Missing packages. Installing them."
+            sudo pacman -S --noconfirm ${MLIST[@]}
+            MLIST=($(comm -23 <(echo "${PKGS_ARCH[@]}" | tr -s ' ' $'\n' | sort -u) <(pacman -Qsq | sort)))
+            if [[ "${#MLIST[@]}" -gt 0 ]] ; then
+                echo "Not all packages installed as planed."
+                return 1
+            fi
+        else
+            echo "All packages are accounted for."
+        fi
+    elif [[ "${#MLIST[@]}" -gt 0 ]] ; then
+        echo "Warning. Missing packages for complete shell experience. Run $(basename $0) to update."
     fi
 }
 
 function alpine-install-pkg() {
     echo "Checking missing packages and installing them..."
+    REPORTCOUNT=0
     for cnt in {1..4} ; do
         case ${cnt} in
             1 ) ALIST=(${PKGS_ALPINE[@]})
@@ -63,46 +71,66 @@ function alpine-install-pkg() {
         esac
 
         MLIST=($(comm -23 <(echo "${ALIST[@]}" | tr -s ' ' $'\n' | sort -u) <(apk list -q | sort)))
-        if [[ "${#MLIST[@]}" -gt 0 ]] ; then
-            echo "Missing packages from repo '${ANAME}'. Installing them."
-            if [[ -z "${AREPO}" ]] ; then
-                sudo apk add ${MLIST[@]}
+        if [[ "${ONLY_REPORT}" == 0 ]] ; then
+            if [[ "${#MLIST[@]}" -gt 0 ]] ; then
+                echo "Missing packages from repo '${ANAME}'. Installing them."
+                if [[ -z "${AREPO}" ]] ; then
+                    sudo apk add ${MLIST[@]}
+                else
+                    apk add --no-cache --repository="${AREPO}" ${MLIST[@]}
+                fi
+                MLIST=($(comm -23 <(echo "${ALIST[@]}" | tr -s ' ' $'\n' | sort -u) <(apk list -q | sort)))
+                if [[ "${#MLIST[@]}" -gt 0 ]] ; then
+                    echo "Not all packages installed as planed."
+                    return 1
+                fi
             else
-                apk add --no-cache --repository="${AREPO}" ${MLIST[@]}
+                echo "All packages are accounted for."
             fi
-			MLIST=($(comm -23 <(echo "${ALIST[@]}" | tr -s ' ' $'\n' | sort -u) <(apk list -q | sort)))
-			if [[ "${#MLIST[@]}" -gt 0 ]] ; then
-				echo "Not all packages installed as planed."
-				return 1
-			fi
         else
-            echo "All packages are accounted for."
+            ((REPORTCOUNT+=${#MLIST[@]}))
         fi
     done
+
+    if [[ "${ONLY_REPORT}" == 0 ]] && [[ "${REPORTCOUNT}" -gt 0 ]] ; then
+        echo "Warning. Missing packages for complete shell experience. Run $(basename $0) to update."
+    fi
 }
 
 function deb-install-pkg() {
     echo "Not yet implemented."
 }
 
-function main() {
-	if echo "$*" | grep -qw 'ot_init' ; then
-		sh -c "$(curl -fsLS https://get.chezmoi.io)" -- init --apply Bubbelb
-	else
+function chezmoi_oneshot() {
+    sh -c "$(curl -fsLS https://get.chezmoi.io)" -- init --one-shot ${CHEZMOI_REPO}
+}
 
-		ran_ok=0
-		if command -v pacman 2>/dev/null ; then
-			arch-install-pkg && ran_ok=1
-		elif command -v apt-get 2>/dev/null ; then
-			deb-install-pkg && ran_ok=1
-		elif command -v apk 2>/dev/null ; then
-			alpine-install-pkg && ran_ok=1
-		fi
+function chezmoi_init() {
+    [[ -d ${HOME}/.local/bin ]] || mkdir -p ${HOME}/.local/bin
+    sh -c "$(curl -fsLS https://get.chezmoi.io)" -- -b ${HOME}/.local/bin init --apply ${CHEZMOI_REPO}
+}
 
-		if [[ "${ran_ok}" == "1" ]] && [[ ! -f /etc/term-config ]] ; then
-			sudo touch /etc/term-config
-		fi
 
+function install-pkg() {
+    ran_ok=0
+    if command -v pacman 2>/dev/null ; then
+        arch-install-pkg && ran_ok=1
+    elif command -v apt-get 2>/dev/null ; then
+        deb-install-pkg && ran_ok=1
+    elif command -v apk 2>/dev/null ; then
+        alpine-install-pkg && ran_ok=1
+    fi
+
+    if [[ "${ran_ok}" == "1" ]] \
+            && [[ "${ONLY_REPORT}" == "0" ]] \
+            && [[ ! -f /etc/term-config ]]
+    then
+        sudo touch /etc/term-config
+    fi
+}
+
+function chezmoi_regular() {
+    if command -v chezmoi 2>/dev/null ; then
 		if [[ -f "${HOME}/.local/share/chezmoi/.git/index" ]] ; then
 			echo "Chezmoi already initialized. Only updating."
 			chezmoi update --apply
@@ -110,7 +138,60 @@ function main() {
 			echo "Chezmoi needs to be initialized."
 			chezmoi init --apply Bubbelb
 		fi
+    else
+        echo "Error: Chezmoi not found. Consider -i, -o, pr -p" >&2
 	fi
 }
 
-main $*
+function show_help() {
+    cat << EOF
+    Terminal Configurer. Setup everything for desired terminal beviour.
+
+    Usage:
+        term-config.sh [-o][-i][-p][-c][-r][-h]
+
+    Parameters:
+        -o  Do a one-shot Chezmoi sync, without installing Chezmoi
+        -i  Install and initialize Chezmoi into homedir (~/.local/bin)
+        -p  Package Install (Needs sudo)
+        -c  Initialize/Update Chezmoi, using installed package
+        -r  Report about missing packages
+        -h  Show this help.
+
+    Notes:
+     - When no parameter is given, options -p and -c are assumed.
+     - The script will only act on the first parameter given. The rest is ignored.
+     - After installing packages, a file '/etc/term-config' is created to tell chezmoi
+        the complete set of packages is installed and the full set of configurations
+        can be applied.
+     - Only the option with no parameters will ask for confirmation to proceed.
+EOF
+}
+
+function print_header() {
+    echo -e "$(basename $0) - Terminal configuration. '$(basename $0) -h' for help.\n" >&2
+}
+
+# Main routine
+
+if [[ -z "$1" ]] ; then
+    print_header
+    read -p "Are you sure to install terminal packages and configure chezmoi? (y/n): " -n 1 -s ANSWER
+    if [[ "${ANSWER^}" == "Y" ]] ; then
+        echo -e "Yes\n"
+        ONLY_REPORT=0
+        install-pkg
+        chezmoi_regular
+    else
+        echo -e "No\n"
+    fi
+else
+    case "$1" in
+        o ) print_header ; chezmoi_oneshot ;;
+        i ) print_header ; chezmoi_init ;;
+        p ) print_header ; ONLY_REPORT=0 ; install-pkg ;;
+        c ) print_header ; chezmoi_regular ;;
+        r ) ONLY_REPORT=1 ; install-pkg ;;
+        * ) print_header ; echo "Unknown parameter." ;;
+    esac
+fi
