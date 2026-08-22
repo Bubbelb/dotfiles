dc91a09cc949ed50c3d0ef568cc51318d876e2ff
Bas Bleeker <1848081+Bubbelb@users.noreply.github.com>
2026-08-22 18:10:09 +0200

    Update .config/fish/conf.d/_manpager_init.fish

--- dot_config/fish/conf.d/_manpager_init.fish
+++ dot_config/fish/conf.d/_manpager_init.fish
@@ -1,12 +1,24 @@
-if type -q most
+if type -q batman
+  set -Ux MANPAGER "batman"
+  set -Ux MANROFFOPT "-c"
+  set -Ux BATMAN_IS_BEING_MANPAGER yes
+else if type -q most
   set -Ux MANPAGER "most"
   set -Ux MANROFFOPT "-c"
+  set -Uu BATMAN_IS_BEING_MANPAGER
   function man
       MANWIDTH=(tput cols) /usr/bin/man $argv
   end
+else if type -q less
+  set -Ux MANPAGER "less --use-color -Dd+r -Du+b"
+  set -gx MANROFFOPT "-P -c"
+  set -Uu BATMAN_IS_BEING_MANPAGER
 end
 
 if type -q bat
-  abbr -a --position anywhere -- --help '--help | bat -plhelp'
-  abbr -a --position anywhere -- -h '-h | bat -plhelp'
+  abbr --add --position anywhere -- --help '--help | bat -plhelp'
+  abbr --add --position anywhere -- -h '-h | bat -plhelp'
+else
+    abbr --erase -h
+    abbr --erase --help
 end
